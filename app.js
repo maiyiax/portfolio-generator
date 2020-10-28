7c06f5dababeeaecfad9c526e8bc233f10f82916
Mai Yia Xiong <maiyiax@gmail.com>
2020-10-28 03:03:41 -0500

    created page-template.js for cleaner coding

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+// console.log(profileDataArgs);
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i += 1) {
+const [name, github] = profileDataArgs;
 
-        console.log(profileDataArr[i]);
-    }
-    console.log('================');
 
-    profileDataArr.forEach((profileItem) => console.log(profileItem));
-};
+// const printProfileData = profileDataArr => {
+//     for (let i = 0; i < profileDataArr.length; i += 1) {
 
-printProfileData(profileDataArgs);
+//         console.log(profileDataArr[i]);
+//     }
+//     console.log('================');
+
+//     profileDataArr.forEach((profileItem) => console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs);
+
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
